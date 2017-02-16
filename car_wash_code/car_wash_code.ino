b19fb182016b84d654517cdef05bd8e5a2d6d5d0
Michael Skjaerlund <michael_b_hansen@live.dk>
2017-02-16 08:43:09 +0100

    First commit of code

---
+++ car_wash_code/car_wash_code.ino
@@ -0,0 +1,30 @@
+/*
+Adafruit Arduino - Lesson 13. DC Motor
+*/
+ 
+int motorPin1 = 3;
+int motorPin2 = 11;
+ 
+void setup() 
+{ 
+  pinMode(motorPin1, OUTPUT);
+  pinMode(motorPin2, OUTPUT);
+  Serial.begin(9600);
+  while (! Serial);
+  Serial.println("Speed 0 to 255");
+} 
+ 
+ 
+void loop() 
+{ 
+  if (Serial.available())
+  {
+    int speed = Serial.parseInt();
+    if (speed >= 0 && speed <= 255)
+    {
+      Serial.println(speed);
+      analogWrite(motorPin1, speed);
+      analogWrite(motorPin2, speed);
+    }
+  }
+} 
